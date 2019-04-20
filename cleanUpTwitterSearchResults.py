cdd309e1f0b3cb610454cfac30207edaf85c2532
timbauer <timbauer@ymail.com>
2019-04-20 14:25:16 -0700

    app now can record company related tweets in individual files with hashtags (the symbol, not the hashtag text) removed.

---
+++ cleanUpTwitterSearchResults.py
@@ -0,0 +1,106 @@
+import csv
+
+companiesTracked = ["Tesla", "Google", "Apple", "CVS Health", "Verizon", "Facebook", "Amazon", "General Motors",
+                    "Chevron", "J.P. Morgan Chase"]
+
+
+def recordTweetRegardingTesla(csvLine):
+    with open ("tweetsRegardingTesla.csv", 'a') as currentCompanyFile:
+        csvWriter = csv.writer(currentCompanyFile)
+        csvWriter.writerow(csvLine)
+        
+def recordTweetRegardingGoogle(csvLine):
+    with open ("tweetsRegardingGoogle.csv", 'a') as currentCompanyFile:
+        csvWriter = csv.writer(currentCompanyFile)
+        csvWriter.writerow(csvLine)
+        
+def recordTweetRegardingApple(csvLine):
+    with open ("tweetsRegardingApple.csv", 'a') as currentCompanyFile:
+        csvWriter = csv.writer(currentCompanyFile)
+        csvWriter.writerow(csvLine)
+        
+def recordTweetRegardingCVS_Health(csvLine):
+    with open ("tweetsRegardingCVS_Health.csv", 'a') as currentCompanyFile:
+        csvWriter = csv.writer(currentCompanyFile)
+        csvWriter.writerow(csvLine)
+        
+def recordTweetRegardingVerizon(csvLine):
+    with open ("tweetsRegardingVerizon.csv", 'a') as currentCompanyFile:
+        csvWriter = csv.writer(currentCompanyFile)
+        csvWriter.writerow(csvLine)
+        
+def recordTweetRegardingFacebook(csvLine):
+    with open ("tweetsRegardingFacebook.csv", 'a') as currentCompanyFile:
+        csvWriter = csv.writer(currentCompanyFile)
+        csvWriter.writerow(csvLine)
+        
+def recordTweetRegardingAmazon(csvLine):
+    with open ("tweetsRegardingAmazon.csv", 'a') as currentCompanyFile:
+        csvWriter = csv.writer(currentCompanyFile)
+        csvWriter.writerow(csvLine)
+        
+def recordTweetRegardingGm(csvLine):
+    with open ("tweetsRegardingGm.csv", 'a') as currentCompanyFile:
+        csvWriter = csv.writer(currentCompanyFile)
+        csvWriter.writerow(csvLine)
+        
+def recordTweetRegardingChevron(csvLine):
+    with open ("tweetsRegardingChevron.csv", 'a') as currentCompanyFile:
+        csvWriter = csv.writer(currentCompanyFile)
+        csvWriter.writerow(csvLine)
+        
+def recordTweetRegardingChase(csvLine):
+    with open ("tweetsRegardingChase.csv", 'a') as currentCompanyFile:
+        csvWriter = csv.writer(currentCompanyFile)
+        csvWriter.writerow(csvLine)
+
+
+rawTweetFile = open("rawTweetFile.csv", 'r')
+csvReader = csv.reader(rawTweetFile)
+
+for line in csvReader:
+
+    line[5] = line[5].replace('#', '')
+
+    if line[0] == companiesTracked[0]:
+        del line[0]
+        recordTweetRegardingTesla(line)
+
+    elif line[0] == companiesTracked[1]:
+        del line[0]
+        recordTweetRegardingGoogle(line)
+
+    elif line[0] == companiesTracked[2]:
+        del line[0]
+        recordTweetRegardingApple(line)
+
+    elif line[0] == companiesTracked[3]:
+        del line[0]
+        recordTweetRegardingCVS_Health(line)
+
+    elif line[0] == companiesTracked[4]:
+        del line[0]
+        recordTweetRegardingVerizon(line)
+
+    elif line[0] == companiesTracked[5]:
+        del line[0]
+        recordTweetRegardingFacebook(line)
+
+    elif line[0] == companiesTracked[6]:
+        del line[0]
+        recordTweetRegardingAmazon(line)
+
+    elif line[0] == companiesTracked[7]:
+        del line[0]
+        recordTweetRegardingGm(line)
+
+    elif line[0] == companiesTracked[8]:
+        del line[0]
+        recordTweetRegardingChevron(line)
+
+    elif line[0] == companiesTracked[9]:
+        del line[0]
+        recordTweetRegardingChase(line)
+
+    else:
+        pass
