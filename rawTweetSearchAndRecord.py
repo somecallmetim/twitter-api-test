b824f2074e71d4dc756c244149294e7dd92eb04b
Tim Bauer <timbauer@ymail.com>
2019-04-24 18:27:49 -0700

    error messages are now logged on new line

--- rawTweetSearchAndRecord.py
+++ rawTweetSearchAndRecord.py
@@ -41,7 +41,7 @@ try:
 
         # records raw tweets and other data in our rawTweetFile
         for tweet in tweepy.Cursor(api.search, q=searchTerm + " -filter:retweets", tweet_mode='extended',
-                                   lang='en').items(70):
+                                   lang='en').items(80):
             # makes sure a tweet isn't a reply to some other tweet and that the tweet isn't a duplicate
             if tweet.in_reply_to_status_id is None and tweet.id not in currentlyRecordedTweetsById:
                 csvWriter.writerow([searchTerm, tweet.id, tweet.user.screen_name, tweet.user.followers_count,
@@ -51,7 +51,7 @@ try:
                 numOfTweetsRecorded += 1
 except Exception as e:
     with open("logFile.txt", 'a+') as logFile:
-        logFile.write(e)
+        logFile.write("\n" + str(e))
 
 
 rawTweetFile.close()
