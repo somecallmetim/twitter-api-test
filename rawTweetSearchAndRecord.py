a313b2a30e5614f7c5320d2a4077bf2ec9ab5052
Tim Bauer <timbauer@ymail.com>
2019-04-24 17:54:56 -0700

    added simple logging feature to keep an eye on twitter api errors

--- rawTweetSearchAndRecord.py
+++ rawTweetSearchAndRecord.py
@@ -14,6 +14,7 @@ companiesTracked = ["Tesla", "Google", "Apple", "CVS Health", "Verizon", "Facebo
                     "Chevron", "J.P. Morgan Chase"]
 
 currentlyRecordedTweetsById = []
+numOfTweetsRecorded = 0
 
 rawTweetFileExists = os.path.isfile('./rawTweetFile.csv')
 
@@ -32,19 +33,30 @@ rawTweetFile = open("rawTweetFile.csv", 'a+')
 csvWriter = csv.writer(rawTweetFile)
 
 
+try:
+    # searches for tweets related to each corporation we're tracking
+    for company in companiesTracked:
 
-# searches for tweets related to each corporation we're tracking
-for company in companiesTracked:
+        searchTerm = company
 
-    searchTerm = company
+        # records raw tweets and other data in our rawTweetFile
+        for tweet in tweepy.Cursor(api.search, q=searchTerm + " -filter:retweets", tweet_mode='extended',
+                                   lang='en').items(70):
+            # makes sure a tweet isn't a reply to some other tweet and that the tweet isn't a duplicate
+            if tweet.in_reply_to_status_id is None and tweet.id not in currentlyRecordedTweetsById:
+                csvWriter.writerow([searchTerm, tweet.id, tweet.user.screen_name, tweet.user.followers_count,
+                                    tweet.created_at, tweet.full_text])
+                # add tweet id to our list to make sure we don't record the same tweet twice
+                currentlyRecordedTweetsById.append(tweet.id)
+                numOfTweetsRecorded += 1
+except Exception as e:
+    with open("logFile.txt", 'a+') as logFile:
+        logFile.write(e)
 
-    # records raw tweets and other data in our rawTweetFile
-    for tweet in tweepy.Cursor(api.search, q = searchTerm + " -filter:retweets", tweet_mode ='extended', lang = 'en').items(60):
-        # makes sure a tweet isn't a reply to some other tweet and that the tweet isn't a duplicate
-        if tweet.in_reply_to_status_id is None and tweet.id not in currentlyRecordedTweetsById:
-            csvWriter.writerow([searchTerm, tweet.id, tweet.user.screen_name, tweet.user.followers_count,
-                                tweet.created_at, tweet.full_text])
-            # add tweet id to our list to make sure we don't record the same tweet twice
-            currentlyRecordedTweetsById.append(tweet.id)
 
 rawTweetFile.close()
+
+# used to keep track of how many tweets we're getting each time this is run
+logFile = open("logFile.txt", 'a+')
+logFile.write("\n" + str(numOfTweetsRecorded))
+logFile.close()
