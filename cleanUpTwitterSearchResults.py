5ffed96bb11f6eb080d1cd6736c6a441cb5f3228
timbauer <timbauer@ymail.com>
2019-05-22 11:28:03 -0700

    Added feature to track number of tweets per company

--- cleanUpTwitterSearchResults.py
+++ cleanUpTwitterSearchResults.py
@@ -4,6 +4,9 @@ import preprocessor as p
 companiesTracked = ["Tesla", "Google", "Apple", "CVS Health", "Verizon", "Facebook", "Amazon", "General Motors",
                     "Chevron", "J.P. Morgan Chase"]
 
+tweetsPerCompany = {"Tesla": 0, "Google": 0, "Apple": 0, "CVS Health": 0, "Verizon": 0, "Facebook": 0, "Amazon": 0, "General Motors": 0,
+                    "Chevron": 0, "J.P. Morgan Chase": 0}
+
 
 def recordTweetRegardingTesla(csvLine):
     with open ("tweetsRegardingTesla.csv", 'a') as currentCompanyFile:
@@ -73,42 +76,55 @@ for line in csvReader:
     if line[0] == companiesTracked[0]:
         del line[0]
         recordTweetRegardingTesla(line)
+        tweetsPerCompany[companiesTracked[0]] += 1
 
     elif line[0] == companiesTracked[1]:
         del line[0]
         recordTweetRegardingGoogle(line)
+        tweetsPerCompany[companiesTracked[1]] += 1
 
     elif line[0] == companiesTracked[2]:
         del line[0]
         recordTweetRegardingApple(line)
+        tweetsPerCompany[companiesTracked[2]] += 1
 
     elif line[0] == companiesTracked[3]:
         del line[0]
         recordTweetRegardingCVS_Health(line)
+        tweetsPerCompany[companiesTracked[3]] += 1
 
     elif line[0] == companiesTracked[4]:
         del line[0]
         recordTweetRegardingVerizon(line)
+        tweetsPerCompany[companiesTracked[4]] += 1
 
     elif line[0] == companiesTracked[5]:
         del line[0]
         recordTweetRegardingFacebook(line)
+        tweetsPerCompany[companiesTracked[5]] += 1
 
     elif line[0] == companiesTracked[6]:
         del line[0]
         recordTweetRegardingAmazon(line)
+        tweetsPerCompany[companiesTracked[6]] += 1
 
     elif line[0] == companiesTracked[7]:
         del line[0]
         recordTweetRegardingGm(line)
+        tweetsPerCompany[companiesTracked[7]] += 1
 
     elif line[0] == companiesTracked[8]:
         del line[0]
         recordTweetRegardingChevron(line)
+        tweetsPerCompany[companiesTracked[8]] += 1
 
     elif line[0] == companiesTracked[9]:
         del line[0]
         recordTweetRegardingChase(line)
+        tweetsPerCompany[companiesTracked[9]] += 1
 
     else:
         pass
+
+for company in tweetsPerCompany:
+    print(company + ": " + str(tweetsPerCompany[company]))
